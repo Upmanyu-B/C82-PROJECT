da00c4acc44accb5bd366d3462b0d99e6a9bf530
Upmanyu-B <139064550+Upmanyu-B@users.noreply.github.com>
2024-05-28 09:11:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log("My first console project");
+
+            var first_name = "1";
+            var last_name = "0";
+            console.log(first_name.concat last_name)
+      var full_name = "Upmanyu Bhardwaj 10";
+            console.log(full_name);
+            var a = ;
+            var b = 5;
+/*
+Create a variable named add and store the addition value of a and b in that variable.
+
+Type: var add = a + b
+
+Then, print the variable to display the addition value
+
+Type: console.log(add)
+*/
+            var add =   ;
+            console.log( );
+
+/*
+Create a variable named sub and store the subtraction value of a and b in that variable.
+
+Type: var sub = a - b
+
+Then, print the variable to display the subtraction value
+
+Type: console.log(sub)
+*/
+            var sub =   ;
+            console.log( );
+
+/*
+Create a variable named multiple and store the multiplication value of a and b in that variable.
+
+Type: var multiple = a * b
+
+Then, print the variable to display the multiplication value
+
+Type: console.log(multiple)
+*/
+            var multiple =   ;
+            console.log();
+
+            var divide = a / b ;
+            console.log(divide);
